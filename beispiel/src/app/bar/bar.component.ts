21f0a07b7c79d64e8bd412d06dc91fc3f75ef7a8
sarkandi <ssarkandi@hotmail.com>
2022-10-17 18:28:06 +0200

    neu
    neue Kompenente

---
+++ beispiel/src/app/bar/bar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit ,Input} from '@angular/core';
+
+@Component({
+  selector: 'bar-component',
+  templateUrl: './bar.component.html',
+  styleUrls: ['./bar.component.css']
+})
+export class BarComponent implements OnInit {
+   wieOft: number = 1;
+   text: string = 'ich komme aus der componente';
+//   @Input() myurl:string;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  click(){
+    this.text= 'ich komme aus der componente und wurde veraendert ' + this.wieOft++;
+    }
+
+  click2(meinText:string){
+  this.text= 'ich komme aus der componente und wurde veraendert ' + this.wieOft++ +' '+ meinText;
+  }
+
+}
