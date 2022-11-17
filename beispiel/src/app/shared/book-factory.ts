2920a2666157fe5b9a104de30dcdfda7ff763522
Lien <ayalp.ender@gmail.com>
2022-11-17 10:48:40 +0100

    fixed bookfactory

--- beispiel/src/app/shared/book-factory.ts
+++ beispiel/src/app/shared/book-factory.ts
@@ -1,21 +1,27 @@
-import { Book} from "./book";
-import { BookRaw} from "./book-raw";
+import {Book} from "./book";
+import {BookRaw} from "./book-raw";
 
 export class BookFactory {
-// static fromRaw(b:BookRaw):Book {
-  static empty (): Book{
-  return{
-    ...b,
-    isbn: '',
-    title: '',
-    authors: [''],
-    published: new Date(),
-    subtitle: '',
-    rating: 0,
-    thumbnails:[
-      { url: '', title: ''}
-    ],
-    description: ''};
-     published: new Date(b.published)
+
+  static fromRaw(b: BookRaw): Book {
+    return {
+      ...b,
+      published: new Date(b.published)
+    };
+  }
+
+  static empty(): Book {
+    return {
+      isbn: '',
+      title: '',
+      authors: [''],
+      published: new Date(),
+      subtitle: '',
+      rating: 0,
+      thumbnails: [
+        {url: '', title: ''}
+      ],
+      description: ''
+    };
   };
 }
