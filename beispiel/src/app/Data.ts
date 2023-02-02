3c1bc339e24d17e029e6d9f53e10a295dce6b92a
sarkandi <ssarkandi@hotmail.com>
2023-02-02 11:01:58 +0100

    addEmptySkill

--- beispiel/src/app/Data.ts
+++ beispiel/src/app/Data.ts
@@ -2,9 +2,28 @@ export interface Skill {
   name: string;
   years: string;
   lastUsed: string;
-  id: number;
+  id?: number;
   currentRate: number;
   stars: number;
+
+}
+
+export class SkillClass implements Skill {
+  name: string;
+  years: string;
+  lastUsed: string;
+  id?: number;
+  currentRate: number;
+  stars: number;
+
+  constructor(skill: Skill) {
+    this.name = skill.name;
+    this.years = skill.years;
+    this.lastUsed = skill.lastUsed;
+    this.id = skill.id;
+    this.currentRate = skill.currentRate;
+    this.stars = skill.stars;
+  }
 }
 
 export interface Faehigkeit {
--- beispiel/src/app/tabelleskills/tabelleskills.component.ts
+++ beispiel/src/app/tabelleskills/tabelleskills.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {SkillsService} from "../tabelleskills.service";
-import {Skill} from "../Data";
+import {Skill, SkillClass} from "../Data";
 import {HttpClient} from '@angular/common/http';
+import {filter, map} from 'rxjs/operators';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-tabelleskills',
@@ -16,6 +18,8 @@ export class TabelleskillsComponent implements OnInit {
 
   currentRate?: Skill;
 
+private url = 'https://6388bc57a4bb27a7f79036af.mockapi.io/lebenslauf/skill/';
+
 
   constructor(public tabelleskills: SkillsService, private http: HttpClient) {
     this.skills = [];
@@ -25,40 +29,70 @@ export class TabelleskillsComponent implements OnInit {
     this.tabelleskills.getAllSkills().subscribe((skills: Skill[]) => this.skills = skills);
   }
 
-  update() {
 
+  update() {
+  console.log('update');
     const change = {
 
       name: this.editableSkill?.name,
       years: this.editableSkill?.years,
       lastUsed: this.editableSkill?.lastUsed,
-      stars: this.editableSkill?.stars  //****
+      stars: this.editableSkill?.stars
 
     };
 
     return this.http.put<Skill>(`https://6388bc57a4bb27a7f79036af.mockapi.io/lebenslauf/skill/${this.editableSkill?.id}`, change)
       .subscribe((newSkill: Skill) =>
         this.skills = this.skills.map(tableSkill => {
-            return tableSkill.id === newSkill.id ? newSkill : tableSkill;
+            return tableSkill.id === newSkill.id ? newSkill : tableSkill;     //man kann update machen mit put. man kann mit post daten geschrieben werden
           }
         )
       );
   }
 
+
   onEdit(skill: Skill) {
     this.editableSkill = skill;
-    // this.editableSkill == this.currentRate;
     return this.currentRate === skill
   }
+
 
   cancelEdit() {
     this.editableSkill = undefined;
   }
 
 
+  deleteSkill(skill) {
+
+    this.http.delete(this.url + '/' + skill.id).subscribe(response => { console.log(response);
+    let index = this.skills.indexOf(skill);
+    this.skills.splice(index, 1);
+    })
+  }
+
+
+  addEmptySkillToSkills() {
+    const emptySkill = new SkillClass({
+      name: "",
+      years: "",
+      lastUsed: "",
+      currentRate: 0,
+      stars: 0
+    });
+
+    this.http.post<Skill>('https://6388bc57a4bb27a7f79036af.mockapi.io/lebenslauf/skill', emptySkill)
+      .pipe(
+        map(addSkill => {
+          this.skills.push(addSkill);
+          return this.skills;
+        })
+      )
+      .subscribe();
+  }
+
+
   isEditable(skill: Skill) {
         return this.editableSkill === skill;
-
   }
 
   isCurrent(skill: Skill) {
